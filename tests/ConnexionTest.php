a3967ae7bd5909a4d0f42167d4b8e900b473df55
dev8Zakaria <zakariarifi8@gmail.com>
2025-10-24 12:16:33 +0100

    Simplify test to check .env instead of .env.example

--- tests/ConnexionTest.php
+++ tests/ConnexionTest.php
@@ -3,6 +3,6 @@ use PHPUnit\Framework\TestCase;
 
 final class ConnexionTest extends TestCase {
     public function testEnvFileExists(): void {
-        $this->assertFileExists(__DIR__ . '/../.env.example');
+        $this->assertFileExists(__DIR__ . '/../.env');
     }
 }
